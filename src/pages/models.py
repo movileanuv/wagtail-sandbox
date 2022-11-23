30647a5e5a5dc68a294db47b2d94157aaaaea094
Victor <victor.movileanu@check24.de>
2022-11-23 10:06:42 +0100

    add StreamField for body
    https://docs.wagtail.org/en/stable/topics/streamfield.html

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,11 +1,20 @@
+from wagtail import blocks
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
     subheading = RichTextField(blank=True)
+    body = StreamField([
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('document', DocumentChooserBlock())
+    ], use_json_field=True, null=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('subheading')
+        FieldPanel('subheading'),
+        FieldPanel('body'),
     ]
